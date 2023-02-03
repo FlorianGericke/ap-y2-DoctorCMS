76eec36e7d2632e8255e4d445775c1b865e1de34
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-02-03 17:58:01 +0100

    added createdAt, deletedAt, updatedAt & state in tables, and added missing foreignKeys, added new mysqldump to repo

--- dms_localhost-2023_02_03_17_54_43-dump.sql
+++ dms_localhost-2023_02_03_17_54_43-dump.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.32, for macos13.0 (arm64)
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (arm64)
 --
 -- Host: 127.0.0.1    Database: dms
 -- ------------------------------------------------------
@@ -28,6 +28,10 @@ CREATE TABLE `addresses` (
   `house_number` varchar(100) DEFAULT NULL,
   `zip` int NOT NULL,
   `location` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -52,7 +56,20 @@ CREATE TABLE `appointments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `start_time` datetime NOT NULL,
   `end_time` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `patient_id` int DEFAULT NULL,
+  `doctor_id` int DEFAULT NULL,
+  `facility_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `appointments_doctors_id_fk` (`doctor_id`),
+  KEY `appointments_facilities_id_fk` (`facility_id`),
+  KEY `appointments_patients_insurance_number_fk` (`patient_id`),
+  CONSTRAINT `appointments_doctors_id_fk` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`),
+  CONSTRAINT `appointments_facilities_id_fk` FOREIGN KEY (`facility_id`) REFERENCES `facilities` (`id`),
+  CONSTRAINT `appointments_patients_insurance_number_fk` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`insurance_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -75,6 +92,10 @@ DROP TABLE IF EXISTS `departments`;
 CREATE TABLE `departments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -99,6 +120,10 @@ CREATE TABLE `doctors` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(100) NOT NULL /*!80023 INVISIBLE */,
   `last_name` int NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -122,6 +147,10 @@ DROP TABLE IF EXISTS `facilities`;
 CREATE TABLE `facilities` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -136,6 +165,36 @@ LOCK TABLES `facilities` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `facility_address`
+--
+
+DROP TABLE IF EXISTS `facility_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facility_address` (
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `facility_id` int DEFAULT NULL,
+  `address_id` int DEFAULT NULL,
+  KEY `facility_address_addresses_id_fk` (`address_id`),
+  KEY `facility_address_facilities_id_fk` (`facility_id`),
+  CONSTRAINT `facility_address_addresses_id_fk` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`),
+  CONSTRAINT `facility_address_facilities_id_fk` FOREIGN KEY (`facility_id`) REFERENCES `facilities` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facility_address`
+--
+
+LOCK TABLES `facility_address` WRITE;
+/*!40000 ALTER TABLE `facility_address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `facility_address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `facility_department`
 --
 
@@ -146,6 +205,10 @@ CREATE TABLE `facility_department` (
   `facility_id` int DEFAULT NULL,
   `department_id` int DEFAULT NULL,
   `doctor_id` int DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updateAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
   KEY `facility_department_departments_id_fk` (`department_id`),
   KEY `facility_department_facilities_id_fk` (`facility_id`),
   KEY `facility_department_doctors_id_fk` (`doctor_id`),
@@ -177,7 +240,14 @@ CREATE TABLE `patients` (
   `last_name` varchar(100) NOT NULL,
   `age` int NOT NULL,
   `phone` varchar(100) NOT NULL,
-  PRIMARY KEY (`insurance_number`)
+  `address_id` int DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`insurance_number`),
+  KEY `patients_addresses_id_fk` (`address_id`),
+  CONSTRAINT `patients_addresses_id_fk` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -199,4 +269,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-03 14:38:59
+-- Dump completed on 2023-02-03 17:54:43
