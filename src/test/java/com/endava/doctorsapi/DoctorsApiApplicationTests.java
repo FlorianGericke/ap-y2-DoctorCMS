18c50b658b4fedfe1cbb54d67face66a141a254e
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-02-03 12:12:16 +0100

    initialize spring application

---
+++ src/test/java/com/endava/doctorsapi/DoctorsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.doctorsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
