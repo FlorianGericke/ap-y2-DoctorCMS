a315b37ad4622990438226d3d7f2c995da044b7f
fgericke <florian.gericke@endava.com>
2023-02-23 11:57:26 +0100

    improved Doctor Controller with validateAndDo function

--- src/main/java/com/endava/doctorsapi/tables/doctor/DoctorController.java
+++ src/main/java/com/endava/doctorsapi/tables/doctor/DoctorController.java
@@ -1,5 +1,7 @@
 package com.endava.doctorsapi.tables.doctor;
 
+import com.endava.doctorsapi.tables.address.Address;
+import com.endava.doctorsapi.tables.address.AddressManagementException;
 import com.endava.doctorsapi.tables.general.ControllerBase;
 import com.endava.doctorsapi.tables.general.DeleteAllById;
 import com.endava.doctorsapi.tables.general.EntityStates;
@@ -10,10 +12,11 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("api/v1/doctors")
-public class DoctorController extends ControllerBase<Doctor,DoctorRepo,DoctorService> {
+public class DoctorController extends ControllerBase<Doctor, DoctorRepo, DoctorService> {
 
 	@Autowired
 	public DoctorController(DoctorService doctorService) {
@@ -25,24 +28,7 @@ public class DoctorController extends ControllerBase<Doctor,DoctorRepo,DoctorSer
 	                   @RequestParam(name = "firstname", required = false) Optional<String> firstname,
 	                   @RequestParam(name = "lastname", required = false) Optional<String> lastname) {
 
-		if (doc.isPresent() && firstname.isEmpty() && lastname.isEmpty()) {
-			if (doc.get().getFirstName() == null || doc.get().getFirstName().length() < 3 ||
-					doc.get().getLastName() == null || doc.get().getLastName().length() < 3) {
-				throw new DoctorManagementException("Please provide a first name and last name (minimum 3 chars)");
-			}
-			service.postDoctor(doc.get());
-			return;
-		}
-
-		if (doc.isEmpty() && firstname.isPresent() && lastname.isPresent()) {
-			if (firstname.get().length() < 3 || lastname.get().length() < 3) {
-				throw new DoctorManagementException("Please provide a first name and last name (minimum 3 chars)");
-			}
-			service.postDoctor(firstname.get(), lastname.get());
-			return;
-		}
-
-		throw new DoctorManagementException("Please provide a RequestBody firstname and lastname or Query Params with them");
+		validateAndDo(doc,firstname,lastname,service::postDoctor);
 	}
 
 	@PutMapping("/{id}")
@@ -50,31 +36,43 @@ public class DoctorController extends ControllerBase<Doctor,DoctorRepo,DoctorSer
 	                  @RequestBody(required = false) Optional<Doctor> doc,
 	                  @RequestParam(name = "firstname", required = false) Optional<String> firstname,
 	                  @RequestParam(name = "lastname", required = false) Optional<String> lastname) {
-		if (id == null) {
-			throw new DoctorManagementException("Invalid param id is null");
-		}
 
-		if(doc.isEmpty() && firstname.isPresent() && lastname.isPresent()) {
-			if (service.get(id).getState().equals(EntityStates.DELETED.toString())) {
-				throw new DoctorManagementException("Cannot change a deleted Doctor");
-			}
-			if (firstname.get().length() < 3|| lastname.get().length() < 3) {
+		validateAndDo(doc,firstname,lastname, doctor -> service.put(id,doctor));
+	}
+
+	private void validateAndDo(Optional<Doctor> doc,
+	                           Optional<String> firstname,
+	                           Optional<String> lastname,
+	                           Consumer<Doctor> resolve) {
+
+		if (doc.isPresent() &&
+				firstname.isEmpty() &&
+				lastname.isEmpty()
+		) {
+			if ((doc.get().getFirstName() == null || doc.get().getFirstName().length() < 3) ||
+					(doc.get().getLastName() == null || doc.get().getLastName().length() < 3)
+			) {
 				throw new DoctorManagementException("Please provide a first name and last name (minimum 3 chars)");
 			}
-			service.put(id, firstname.get(), lastname.get());
+
+			resolve.accept(doc.get());
 			return;
 		}
 
-		if(doc.isPresent() && firstname.isEmpty() && lastname.isEmpty()) {
-			if (doc.get().getFirstName() == null || doc.get().getFirstName().length() < 3 ||
-					doc.get().getLastName() == null || doc.get().getLastName().length() < 3) {
+		if (doc.isEmpty() &&
+				firstname.isPresent() &&
+				lastname.isPresent()
+		) {
+			if (firstname.get().length() < 3 ||
+					lastname.get().length() < 3
+			) {
 				throw new DoctorManagementException("Please provide a first name and last name (minimum 3 chars)");
 			}
-			service.put(id, doc.get().getFirstName(), doc.get().getLastName());
+
+			resolve.accept(new Doctor(firstname.get(),lastname.get()));
 			return;
 		}
 
 		throw new DoctorManagementException("Please provide a RequestBody firstname and lastname or Query Params with them");
-
 	}
 }
