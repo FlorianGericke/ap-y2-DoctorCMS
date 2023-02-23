c466bb476357a79ba358cb285821233644b59fd9
fgericke <florian.gericke@endava.com>
2023-02-23 11:34:30 +0100

    id generic to

--- src/main/java/com/endava/doctorsapi/tables/general/ServiceBase.java
+++ src/main/java/com/endava/doctorsapi/tables/general/ServiceBase.java
@@ -6,14 +6,14 @@ import com.endava.doctorsapi.tables.address.AddressManagementException;
 
 import java.util.List;
 
-public class ServiceBase<Type extends EntityBase, Repo extends RepoBase<Type, Long>> {
+public class ServiceBase<Type extends EntityBase,ID, Repo extends RepoBase<Type, ID>> {
 	protected final Repo repo;
 
 	public ServiceBase(Repo repo) {
 		this.repo = repo;
 	}
 
-	public Type get(Long id) {
+	public Type get(ID id) {
 		return repo.findById(id)
 				.orElseThrow(() -> {
 					throw new AddressManagementException("id not found");
@@ -24,11 +24,11 @@ public class ServiceBase<Type extends EntityBase, Repo extends RepoBase<Type, Lo
 		return repo.findAll();
 	}
 
-	public void delete(Long id) {
+	public void delete(ID id) {
 		repo.deleteById(id);
 	}
 
-	public void deleteAllById(Iterable<? extends Long> longs) {
+	public void deleteAllById(Iterable<? extends ID> longs) {
 		repo.deleteAllById(longs);
 	}
 
