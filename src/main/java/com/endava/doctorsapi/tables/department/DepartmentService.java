0a7ee3afb1fc2397ab2ee1dd031a4f00a14d60f6
fgericke <florian.gericke@endava.com>
2023-02-24 12:02:54 +0100

    refactoring DepartmentController and DepartmentService with ControllerBase and ServiceBase

--- src/main/java/com/endava/doctorsapi/tables/department/DepartmentService.java
+++ src/main/java/com/endava/doctorsapi/tables/department/DepartmentService.java
@@ -1,61 +1,37 @@
 package com.endava.doctorsapi.tables.department;
 
 
+import com.endava.doctorsapi.tables.general.base.ServiceBase;
 import com.endava.doctorsapi.tables.general.exceptions.CmsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class DepartmentService {
-    private final DepartmentRepo departmentRepo;
+public class DepartmentService extends ServiceBase<Department, Long, DepartmentRepo> {
 
-    @Autowired
-    public DepartmentService(DepartmentRepo departmentRepo) {
-        this.departmentRepo = departmentRepo;
-    }
+	@Autowired
+	public DepartmentService(DepartmentRepo departmentRepo) {
+		super(departmentRepo);
+	}
 
-    public void postDepartment(String name) {
-        departmentRepo.save(new Department(name));
-    }
-    public void postDepartment(Department department) {
-        this.postDepartment(department.getName());
-    }
+	public void postDepartment(String name) {
+		repo.save(new Department(name));
+	}
 
-    public void put(Long id, String name) {
-        Department department = departmentRepo.findById(id)
-                .orElseThrow(() -> {
-                    throw new CmsException("id not found");
-                });
-        department.setName(name);
-        departmentRepo.save(department);
-    }
+	public void postDepartment(Department department) {
+		this.postDepartment(department.getName());
+	}
 
-    public void put(Long id, Department department) {
-        this.put(id, department.getName());
-    }
+	public void put(Long id, String name) {
+		Department department = repo.findById(id)
+				.orElseThrow(() -> {
+					throw new CmsException("id not found");
+				});
+		department.setName(name);
+		repo.save(department);
+	}
 
-    public Department get(Long id) {
-        return departmentRepo.findById(id)
-                .orElseThrow(() -> {
-                    throw new CmsException("id not found");
-                });
-    }
-
-    public List<Department> getAll() {
-        return departmentRepo.findAll();
-    }
-
-    public void delete(Long id) {
-        departmentRepo.deleteById(id);
-    }
-
-    public void deleteAllById(Iterable<? extends Long> longs) {
-        departmentRepo.deleteAllById(longs);
-    }
-
-    public void deleteAll() {
-        departmentRepo.deleteAll();
-    }
+	public void put(Long id, Department department) {
+		this.put(id, department.getName());
+	}
 }
