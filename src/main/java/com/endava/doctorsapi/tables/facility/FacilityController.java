54833e8a6b15d28a839d79949ff903b61d56eac9
fgericke <florian.gericke@endava.com>
2023-03-13 14:37:24 +0100

    adding post Endpoint for Facilities

--- src/main/java/com/endava/doctorsapi/tables/facility/FacilityController.java
+++ src/main/java/com/endava/doctorsapi/tables/facility/FacilityController.java
@@ -1,16 +1,34 @@
 package com.endava.doctorsapi.tables.facility;
 
+import com.endava.doctorsapi.tables.department.Department;
 import com.endava.doctorsapi.tables.general.base.ControllerBase;
+import com.endava.doctorsapi.tables.general.exceptions.CmsException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/facilitys")
+@RequestMapping("api/v1/facility")
 public class FacilityController extends ControllerBase<Facility, FacilityRepo, FacilityService> {
 
 	@Autowired
 	public FacilityController(FacilityService facilityService) {
 		super(facilityService);
 	}
+
+	@PostMapping()
+	public void onPost(@RequestBody() Optional<Facility> facility) {
+
+		if (facility.isEmpty()) {
+			throw new CmsException("Please provide a RequestBody withe attribute name");
+		}
+
+		if (facility.get().getName() == null || facility.get().getName().length() < 2) {
+			throw new CmsException("Please provide a name (minimum 2 chars)");
+		}
+
+		service.postFacility(facility.get());
+	}
+
 }
