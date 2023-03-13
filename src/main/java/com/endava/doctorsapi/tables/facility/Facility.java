3ebd281f09e611d42d2f1cc94c52858d3509ef96
fgericke <florian.gericke@endava.com>
2023-03-13 14:15:14 +0100

    basic facility service

--- src/main/java/com/endava/doctorsapi/tables/facility/Facility.java
+++ src/main/java/com/endava/doctorsapi/tables/facility/Facility.java
@@ -1,8 +1,45 @@
 package com.endava.doctorsapi.tables.facility;
 
 import com.endava.doctorsapi.tables.general.base.EntityBase;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@Table(name = "facilities")
+@SQLDelete(sql = "UPDATE facilities SET state = 'deleted', deleted_at = current_date WHERE id=?")
+@Where(clause = "state IN ('created', 'updated')")
 public class Facility extends EntityBase {
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	public Facility(String name) {
+		this.name = name;
+	}
+
+	public Facility() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Facility{" +
+				"name='" + name + '\'' +
+				", id=" + id +
+				", createdAt=" + createdAt +
+				", updatedAt=" + updatedAt +
+				", deletedAt=" + deletedAt +
+				", state='" + state + '\'' +
+				'}';
+	}
 }
