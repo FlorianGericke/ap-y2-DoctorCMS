29054e1a26ae3e37aec3f33b5909f9a2ef61b953
fgericke <florian.gericke@endava.com>
2023-03-13 16:37:43 +0100

    fixing endPoints of DoctorController

--- src/main/java/com/endava/doctorsapi/tables/doctor/DoctorController.java
+++ src/main/java/com/endava/doctorsapi/tables/doctor/DoctorController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @RestController
-@RequestMapping("api/v1/doctors")
+@RequestMapping("api/v1/doctor")
 public class DoctorController extends ControllerBase<Doctor, DoctorRepo, DoctorService> {
 
 	@Autowired
@@ -18,55 +17,29 @@ public class DoctorController extends ControllerBase<Doctor, DoctorRepo, DoctorS
 	}
 
 	@PostMapping()
-	public void onPost(@RequestBody(required = false) Optional<Doctor> doc,
-	                   @RequestParam(name = "firstname", required = false) Optional<String> firstname,
-	                   @RequestParam(name = "lastname", required = false) Optional<String> lastname) {
-
-		validateAndDo(doc, firstname, lastname, service::postDoctor);
+	public void onPost(@RequestBody(required = false) Optional<Doctor> doctor) {
+		validate(doctor);
+		service.postDoctor(doctor.get());
 	}
 
 	@PutMapping("/{id}")
-	public void onPut(@PathVariable(value = "id") Long id,
-	                  @RequestBody(required = false) Optional<Doctor> doc,
-	                  @RequestParam(name = "firstname", required = false) Optional<String> firstname,
-	                  @RequestParam(name = "lastname", required = false) Optional<String> lastname) {
+	public void onPut(@PathVariable(value = "id") Long id, @RequestBody() Optional<Doctor> doctor) {
+		if (id == null) {
+			throw new ControllerException(this, "Invalid param id is null");
+		}
 
-		validateAndDo(doc, firstname, lastname, doctor -> service.put(id, doctor));
+		validate(doctor);
+		service.put(id,doctor.get());
 	}
 
-	private void validateAndDo(Optional<Doctor> doc,
-	                           Optional<String> firstname,
-	                           Optional<String> lastname,
-	                           Consumer<Doctor> resolve) {
-
-		if (doc.isPresent() &&
-				firstname.isEmpty() &&
-				lastname.isEmpty()
-		) {
-			if ((doc.get().getFirstName() == null || doc.get().getFirstName().length() < 3) ||
-					(doc.get().getLastName() == null || doc.get().getLastName().length() < 3)
-			) {
-				throw new ControllerException(this, "Please provide a first name and last name (minimum 3 chars)");
-			}
-
-			resolve.accept(doc.get());
-			return;
+	private void validate(Optional<Doctor> doctor) {
+		if (doctor.isEmpty()) {
+			throw new ControllerException(this, "Please provide a RequestBody withe attributs firstName, lastName");
 		}
 
-		if (doc.isEmpty() &&
-				firstname.isPresent() &&
-				lastname.isPresent()
-		) {
-			if (firstname.get().length() < 3 ||
-					lastname.get().length() < 3
-			) {
-				throw new ControllerException(this, "Please provide a first name and last name (minimum 3 chars)");
-			}
-
-			resolve.accept(new Doctor(firstname.get(), lastname.get()));
-			return;
+		if ((doctor.get().getFirstName() == null || doctor.get().getFirstName().length() < 2) ||
+				(doctor.get().getLastName() == null || doctor.get().getFirstName().length() < 2)) {
+			throw new ControllerException(this, "Please provide a First and Lastname in RequestBody (min 2 chars)");
 		}
-
-		throw new ControllerException(this, "Please provide a RequestBody firstname and lastname or Query Params with them");
 	}
 }
