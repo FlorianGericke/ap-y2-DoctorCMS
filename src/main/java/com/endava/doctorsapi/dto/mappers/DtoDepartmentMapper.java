185c9b5dc8873cad3153fdb4385ee9b33531b175
fgericke <florian.gericke@endava.com>
2023-03-17 13:15:28 +0100

    adding DtoDepartmentMapper

--- src/main/java/com/endava/doctorsapi/dto/mappers/DtoDepartmentMapper.java
+++ src/main/java/com/endava/doctorsapi/dto/mappers/DtoDepartmentMapper.java
@@ -1,15 +1,17 @@
 package com.endava.doctorsapi.dto.mappers;
 
+import com.endava.doctorsapi.dto.response.DepartmentResponse;
 import com.endava.doctorsapi.dto.response.DoctorResponse;
+import com.endava.doctorsapi.tables.department.Department;
 import com.endava.doctorsapi.tables.doctor.Doctor;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
-public class DtoDepartmentMapper implements Function<Doctor, DoctorResponse> {
+public class DtoDepartmentMapper implements Function<Department, DepartmentResponse> {
 	@Override
-	public DoctorResponse apply(Doctor doctor) {
-		return new DoctorResponse(doctor.getFirstName(), doctor.getLastName());
+	public DepartmentResponse apply(Department department) {
+		return new DepartmentResponse(department.getName());
 	}
 }
